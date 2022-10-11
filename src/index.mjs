5d01210c7f1a5e148ef992d78b53a4352c537642
ztephm <steph.mongon@datadoghq.com>
2022-10-10 19:17:17 -0600

    switching to cdn sync instrumentation type

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,19 @@
-import { datadogRum } from '@datadog/browser-rum';
+// import { datadogRum } from '@datadog/browser-rum';
 
-datadogRum.init({
-    applicationId: 'ea889122-e180-426d-9be1-086a8bc22008',
-    clientToken: 'pub2e42182bf5a270da1f34684d13f32979',
-    site: 'datadoghq.com',
-    service:'easy-rum-starter-guide-npm',
-    env:'staging-1',
-    // Specify a version number to identify the deployed version of your application in Datadog 
-    version: '1.0.0',
-    sampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel:'mask-user-input'
-}).then(console.log('rum init test'));
+// datadogRum.init({
+//     applicationId: 'ea889122-e180-426d-9be1-086a8bc22008',
+//     clientToken: 'pub2e42182bf5a270da1f34684d13f32979',
+//     site: 'datadoghq.com',
+//     service:'easy-rum-starter-guide-npm',
+//     env:'staging-1',
+//     // Specify a version number to identify the deployed version of your application in Datadog 
+//     version: '1.0.0',
+//     sampleRate: 100,
+//     sessionReplaySampleRate: 20,
+//     trackInteractions: true,
+//     trackResources: true,
+//     trackLongTasks: true,
+//     defaultPrivacyLevel:'mask-user-input'
+// }).then(console.log('rum init test'));
     
-datadogRum.startSessionReplayRecording();
+// datadogRum.startSessionReplayRecording();
